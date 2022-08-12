aadb8a42c21e38a9cd71754997cb785a78476a0f
Ayilk <srangel.h92@gmail.com>
2022-08-11 19:36:48 -0500

    Segundo selector anidado

--- src/app/paises/pages/selector-page/selector-page.component.ts
+++ src/app/paises/pages/selector-page/selector-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { PaisSmall } from '../../interfaces/paises.interface';
 import { PaisesService } from '../../services/paises.service';
 
 @Component({
@@ -12,18 +13,33 @@ export class SelectorPageComponent implements OnInit {
 
   miFormulario: FormGroup = this.fb.group({
     region: ['', Validators.required],
+    pais: ['', Validators.required]
 
   })
 
 
   //llenar selectores
   regiones: string[] = [];
+  paises: PaisSmall[] = [];
+
 
   constructor(private fb: FormBuilder,
               private paisesServce: PaisesService) { }
 
   ngOnInit(): void {
     this.regiones = this.paisesServce.regiones
+
+    //Cuando cambie la region
+    this.miFormulario.get('region')?.valueChanges
+      .subscribe(region => {
+        console.log(region)
+
+        this.paisesServce.getPaisesPorRegion( region )
+          .subscribe(paises => {
+            console.log(paises)
+            this.paises = paises
+          })
+      })
   }
 
   guardar(){
